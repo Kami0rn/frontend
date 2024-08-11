25c525b6746d5afc29f0f1b4c6840408adf31fde
Kami0rn <74646193+Kami0rn@users.noreply.github.com>
2024-08-11 15:06:46 +0700

    Hi

--- src/service/http/Admin.tsx
+++ src/service/http/Admin.tsx
@@ -1,4 +1,4 @@
-const serverAddress = 'https://mee.chatbot.api.thetigerteamacademy.net';
+const serverAddress = 'http://127.0.0.1:5000';
 
 export interface AdminToggle {
     action: string;
--- src/service/http/Chat.tsx
+++ src/service/http/Chat.tsx
@@ -1,5 +1,5 @@
 // service/http/Chat.ts
-const serverAddress = 'https://mee.chatbot.api.thetigerteamacademy.net';
+const serverAddress = 'http://127.0.0.1:5000';
 
 export interface ChatFormData {
   user_input: string;
